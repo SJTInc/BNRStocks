b79b975be6d03c370201940b957a826303c12bea
SJT10 <Home@Thomass-MacBook-Pro.local>
2014-08-04 18:31:15 -0500

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Thomas McCorquodale on 8/4/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
